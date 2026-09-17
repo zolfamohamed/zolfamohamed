c4dd55b0558661ab5597fcb53f038ee516c992b2
zolfamohamed <zolfafarghaly@gmail.com>
2026-09-17 05:24:39 +0300

    Fix GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/zolfamohamed">
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
